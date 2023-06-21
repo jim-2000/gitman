f7dbe09bfe33cc5ecbfc4f1dc3ae97f60c85cdd3
jim-2000 <jimthecaptaionofsill@example.com>
2023-06-21 11:31:22 +0600

    user model define

--- lib/controller/gitController.dart
+++ lib/controller/gitController.dart
@@ -1,1 +1,14 @@
+import 'package:get/get.dart';
 
+class GitController extends GetxController {
+  //
+  final RxBool isLoading = false.obs;
+  //
+  void startLoading() {
+    isLoading.value = true;
+  }
+
+  void stopLoading() {
+    isLoading.value = false;
+  }
+}
